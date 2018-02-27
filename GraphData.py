eb56bf974c8bb5c11978cf2fba464e036dcae5fa
Wouther Bons <wouther.bons@gmail.com>
2018-02-27 15:23:51 +0100

    Initial drawing of graph

--- GraphData.py
+++ GraphData.py
@@ -1,12 +1,15 @@
 import csv
+import networkx as nx
 
 
 class GraphData:
     fileName = ""
+    graphObj = nx.Graph()
 
     def __init__(self, fileName):
         print("constructing new GraphData instance from file", fileName)
         self.loadFile(fileName)
+        # self.graphObj = nx.Graph()
 
     def loadFile(self, fileName):
         # TODO check if file exists and is valid csv file
@@ -14,6 +17,28 @@ class GraphData:
         with open(fileName, 'r') as dataFile:
             dbCSV = csv.reader(dataFile, delimiter='\t', quotechar='"')
             for row in dbCSV:
-                print(",", row)
+                # print(",", row)
+                self.graphObj.add_node(row[0])
+                self.graphObj.add_node(row[1])
+                self.graphObj.add_edge(row[0], row[1], time=row[2])
 
         return True  # TODO don't always return true
+
+    def plotGraph(self):
+        print("plotting graph")
+        print(list(self.graphObj.edges()))
+
+        nx.draw_networkx(self.graphObj)
+
+        plt.figure(figsize=(8, 8))
+        # nx.draw_networkx_edges(G)
+        # nx.draw_networkx_nodes(G,
+        #                        node_size=80)
+        #
+        # plt.xlim(-0.05, 1.05)
+        # plt.ylim(-0.05, 1.05)
+        # plt.axis('off')
+        # plt.show()
+
+    def getGraphObj(self):
+        return self.graphObj
--- __init__.py
+++ __init__.py
@@ -1,8 +1,25 @@
 #__all__ = ['FinDB'] # list of submodules
 import GraphData
+import matplotlib.pyplot as plt
+import networkx as nx
 #from parser import *
 ###import FinDB
 
 print("initializing")
 graphData = GraphData.GraphData("data/Data_Highschool.txt")
+# graphData.plotGraph()
 print("done")
+
+plt.figure(figsize=(10, 10))
+# nx.draw_networkx_edges(G)
+# nx.draw_networkx_nodes(G,
+#                        node_size=80)
+nx.draw(graphData.getGraphObj())
+
+# plt.xlim(-0.05, 1.05)
+# plt.ylim(-0.05, 1.05)
+plt.axis('off')
+plt.show()
+
+
+
