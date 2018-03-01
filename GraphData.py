ca5cfa9be603d272fe26ac30513ab3ff8e0a8ebd
GioBi <gbk@hotmail.it>
2018-03-01 12:50:18 +0100

    methods for loading different graphs updated

--- GraphData.py
+++ GraphData.py
@@ -9,29 +9,54 @@ class GraphData:
     def __init__(self, fileName):
         print("constructing new GraphData instance from file", fileName)
         self.loadFile(fileName)
-        # self.graphObj = nx.Graph()
 
     def loadFile(self, fileName):
         # TODO check if file exists and is valid csv file
         self.fileName = fileName
-        with open(fileName, 'r') as dataFile:
+
+    def loadInstantGraphs(self):
+        self.insGraphs = [];
+        for timeIndex in range(0, 7375+1):
+            self.insGraphs.append( nx.empty_graph() )
+        with open(self.fileName, 'r') as dataFile:
             dbCSV = csv.reader(dataFile, delimiter='\t', quotechar='"')
-            next(dbCSV) # skip first line
+            next(dbCSV)  # skip first line
+            for row in dbCSV:
+                self.insGraphs[ int(row[2])].add_edge(int(row[0]), int(row[1]), t=int(row[2]))
+        return self.insGraphs
+    
+    def getAggregatedGraph(self):
+        return self.graphAgg;
+
+    def loadAggregatedGraph(self):
+        self.graphAgg = nx.empty_graph();
+        # TODO check if file exists and is valid csv file
+        with open(self.fileName, 'r') as dataFile:
+            dbCSV = csv.reader(dataFile, delimiter='\t', quotechar='"')
+            next(dbCSV)  # skip first line
             for row in dbCSV:
                 # If the two nodes were linked already, DO NOT add the new edge.
-                if not self.graphObj.has_edge(int(row[0]),  int(row[1])):
-                    self.graphObj.add_edge(int(row[0]), int(row[1]), t=int(row[2]))
-        return True  # TODO don't always return true
+                if not self.graphAgg.has_edge(int(row[0]), int(row[1])):
+                    self.graphAgg.add_edge(int(row[0]), int(row[1]), t=int(row[2]))
+        return self.graphAgg
 
-    def getGraphAtTime(self, time):
-        # Initialize with empty graph (all nodes, no links)
-        graphAtTime = nx.empty_graph();
-        # Store all edges in a dictionary
-        allEdges = nx.get_edge_attributes(self.graphObj, 't')
-        for edge,currEdgeTime in allEdges.items():
-            if currEdgeTime <= time: # add the edge if the link was made before time 't'
-                graphAtTime.add_edge(edge, currEdgeTime)
-        return graphAtTime
+    # def instantGraph(self, time):
+    #     self.instantGraph = nx.graph();
+    #     next(self.fileName)  # skip first line
+    #     for row in self.file:
+    #         # If the edge was added at time t, add it to the graph
+    #         if int(row[2]) == time:
+    #             self.instantGraph.add_edge(int(row[0]), int(row[1]), t=int(row[2]))
+    #     return self.instantGraph
+    #
+    #     # instantiate an empty graph
+    #     instantGraph = nx.empty_graph();
+    #     # Store all edges in a dictionary
+    #     allEdges = nx.get_edge_attributes(self.graphObj, 't')
+    #     for edge,currEdgeTime in allEdges.items():
+    #         if currEdgeTime <= time: # add the edge if the link was made before time 't'
+    #             instantGraph.add_edge(edge, currEdgeTime)
+    #     return instantGraph
 
     def plotGraph(self):
         print("plotting graph")
@@ -44,12 +69,11 @@ class GraphData:
         plt.axis('off')
         plt.show()
 
-    def plotGraph(self):
+    def plotGraphAgg(self):
         print("plotting graph")
-        print(list(self.graphObj.edges()))
-
+        print(list(self.graphAgg.edges()))
         plt.figure(figsize=(8, 8))
-        nx.draw_networkx(self.graphObj, pos=nx.circular_layout(self.graphObj), node_size=5)
+        nx.draw_networkx(self.graphAgg, pos=nx.circular_layout(self.graphAgg), node_size=5)
         # plt.xlim(-0.05, 1.05)
         # plt.ylim(-0.05, 1.05)
         plt.axis('off')
@@ -57,12 +81,8 @@ class GraphData:
 
     def plotGraph(self, g):
         print("plotting graph")
-        plt.figure(figsize=(8, 8))
         nx.draw_networkx(g, pos=nx.circular_layout(g), node_size=5)
         # plt.xlim(-0.05, 1.05)
         # plt.ylim(-0.05, 1.05)
         plt.axis('off')
         plt.show()
-
-    def getGraphObj(self):
-        return self.graphObj
--- partB.py
+++ partB.py
@@ -4,19 +4,24 @@ import networkx as nx
 import numpy
 
 print("initializing")
-graphData = GraphData.GraphData("data/Data_Highschool.txt")
+gData = GraphData.GraphData("data/Data_Highschool.txt")
+gInst = gData.loadInstantGraphs();
+gAgg = gData.loadAggregatedGraph()
+
 print("done")
 
-G = graphData.getGraphObj()
+f = plt.figure(figsize=(8, 8))
+pl = nx.draw_shell(gAgg, node_size=1, width=0.02)
+plt.show()
+f = plt.figure(figsize=(8, 8))
 
 for i in range(1,10):
-    Gt = graphData.getGraphAtTime(i)
-    graphData.plotGraph(Gt)
-    print(Gt.number_of_edges())
-    print(Gt.number_of_nodes())
-
-Gt = graphData.getGraphAtTime(7595);
-
-print(Gt.number_of_edges())
-print(Gt.number_of_nodes())
+    f.clear();
+    pl = nx.draw_shell(gInst[i], node_size=5)
+#
+#
+# Gt = graphData.getGraphAtTime(7595);
+#
+# print(Gt.number_of_edges())
+# print(Gt.number_of_nodes())
 
